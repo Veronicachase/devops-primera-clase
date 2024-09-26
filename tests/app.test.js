03f314f3f5bb83baca6466fac8377ccef7191471
Veronicachase <chase.veronica@gmail.com>
2024-09-26 19:33:16 +0200

    x

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('API Tests', () => {
-    it('should return Hello World from mailService on GET /', async () => {
+    it('should return Hello World from mi casa on GET /', async () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
-        expect(response.body.message).toBe('Hello World from mailService!');
+        expect(response.body.message).toBe('Hello World from mi casa!');
     });
 
     it('should echo back the posted data on POST /echo', async () => {
